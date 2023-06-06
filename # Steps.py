58354f0c3aedef6b530d6e110f4b6023474e3e73
mfdalgetty <107519009+mfdalgetty@users.noreply.github.com>
2023-06-06 09:41:38 -0700

    Steps

---
+++ # Steps.py
@@ -0,0 +1,8 @@
+# Steps
+
+# 1. Host database on AWS
+# 2. Connect on Psycopg2
+# 3. Build a simple webapp
+# 4. Expand webapp
+# 5. Create trigger, stored procedure
+# 6. Host webapp on AWS
--- hello.py
+++ hello.py
@@ -1,9 +1,33 @@
-from flask import Flask
+
+
+
+
+
+from flask import Flask, render_template, request, redirect, url_for, flash
+import psycopg2
+import psycopg2.extras
 
 app = Flask(__name__)
+app.secret_key = "bananabread"
+
+connection = psycopg2.connect(user='postgres',
+                                password='Oftheworld1!',
+                                host='localhost',
+                                port='5432',
+                                database='gbts')
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    t = "SELECT * FROM Tree"
+    cursor.execute(t)
+    list_users = cursor.fetchall()
+    # return render_template('index.html', list_users = list_users)
+    print(list_users)
+    return(list_users)
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
+
+
+
